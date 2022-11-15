acd25c864d723283757438cec852d35a32c85c9d
anoveskey <nexient.adam.noveskey@dealerware.com>
2022-11-15 16:35:42 -0500

    Set up the basic webpack and git repo scaffolding for new version of evbpmusic.com

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const helloDiv = document.createElement('div');
+helloDiv.innerHTML = "Here, you will find all things EVBP-related. Take some time and have a look around!";
+document.body.append(helloDiv);
