448f89d9a56b9a2b13b000726ebebcc8cc9815a5
anoveskey <nexient.adam.noveskey@dealerware.com>
2022-11-15 16:57:27 -0500

    Added .idea to gitignore, added HtmlWebpackPlugin to webpack config, also added a build script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
+    mode: 'development',
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "build"),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+    ],
 };
