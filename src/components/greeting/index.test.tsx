20dbee157851477e8d741b96d4719c8396c9367f
anoveskey <nexient.adam.noveskey@dealerware.com>
2022-11-18 15:48:04 -0500

    Fixed the failing unit test. Pushing up my changes so I can refer back to them.

--- src/components/greeting/index.test.tsx
+++ src/components/greeting/index.test.tsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Greeting from './index';
@@ -6,6 +7,7 @@ import Greeting from './index';
 describe('Greeting', () => {
    it('should render', () => {
       render(<Greeting />);
-      expect(screen.getByRole("p")).toBeInTheDocument();
+      const paragraph = screen.getByText("Here is a greeting paragraph that I made", {exact: false});
+      expect(paragraph).toBeInTheDocument();
    });
 });
