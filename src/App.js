85c774c56460e9eadc4ef2475f87d3d6f2cf0ac0
anoveskey <nexient.adam.noveskey@dealerware.com>
2022-11-22 14:59:00 -0500

    Fixed the Routing component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useRoutes,
+} from 'react-router-dom';
 import Greeting from './components/greeting';
 
-const App = () => (
-    <Routes>
-        <Route path="/" component={Greeting} />
-        <Route path="/discography" component={<Greeting name="discography" />} />
-        <Route path="/shows" component={<Greeting name="shows" />} />
-        <Route component={<Greeting name="404" />} />
-    </Routes>
-);
+const App = () => {
+    let routes = useRoutes([
+        { path: "/", element: <Greeting />},
+        { path: "discography", element: <Greeting name="discography" />},
+        { path: "shows", element: <Greeting name="shows" />},
+        { path: "*", element: <Greeting name="404" />},
+    ]);
 
-export default App;
+    return routes;
+};
+
+const AppWrapper = () => {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+}
+
+export default AppWrapper;
